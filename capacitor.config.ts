639a20bb9166f862e797c75e3212003323c2d369
Varun P Nayak <58069627+NOTvaRUN@users.noreply.github.com>
2025-07-07 14:59:54 +0530

    Home Screen Design updated.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'scanner',
+  appId: 'io.twms.poc',
+  appName: 'TWMS POC',
   webDir: 'www'
 };
 
--- src/app/view-message/view-message.module.ts
+++ src/app/view-message/view-message.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ViewMessagePage } from './view-message.page';
-
 import { IonicModule } from '@ionic/angular';
-
 import { ViewMessagePageRoutingModule } from './view-message-routing.module';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ViewMessagePageRoutingModule
+    ViewMessagePageRoutingModule,
+    MatTableModule,
   ],
   declarations: [ViewMessagePage]
 })
